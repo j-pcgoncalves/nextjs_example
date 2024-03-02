05e0299dadfb7d60e3460b97f3c6735274d06eff
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-03-02 15:55:21 +0000

    create Image component

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import Image from "../components/Image";
+
 const About = () => {
     return (
         <div style={{ fontSize: "20px", color: "blue" }}>
@@ -7,6 +9,7 @@ const About = () => {
             <Link href="/">
                 <button>Back</button>
             </Link>
+            <Image />
             <p>I was a magician once</p>
         </div>
     );
